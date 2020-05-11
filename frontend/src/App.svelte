8991cb4fb70bff3895a56fe509a549329c31ebce
Eric S. Londres <elondres@stevens.edu>
2020-05-10 21:47:38 -0400

    nuked everything

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,9 @@
-<script>
- export let name;
-</script>
+<script></script>
 
-<main>
-    <h1>DocPal</h1>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<style lang="sass"></style>
 
-<style type="text/sass">
-main
-  text-align: center
-  padding: 1em
-  max-width: 240px
-  margin: 0 auto
+<header></header>
   
- 
+<main></main>
 
-h1
-  color: #ff3e00;
-  text-transform: uppercase;
-  font-size: 4em;
-  font-weight: 100;
- 
-
-@media (min-width: 640px)
-  main
-    max-width: none
- 
-</style>
+<footer></footer>
