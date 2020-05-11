94205f3233677c0f3997b5d39865d895ad57672a
ebuczek525 <ebuczek@stevens.edu>
2020-05-11 19:28:38 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const configRoutes = require('./routes');
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(session({
+  name: 'AuthCookie',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: true
+}));
+
 configRoutes(app);
 
 app.listen(3000, () => {
