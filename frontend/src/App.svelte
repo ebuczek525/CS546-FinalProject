6b301dc3cfc4ee1058c1bdc56a26b53c71276cba
Eric S. Londres <elondres@stevens.edu>
2020-05-12 13:56:47 -0400

    whitespace

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,20 @@
 <script>
-  import Editor from './Editor.svelte';
+ import Editor from './Editor.svelte';
+ 
 </script>
 
 <style lang="sass">
-h1
-  text-align: center
-main
-  height: 100%
-  width: 100%
-  margin: 0
-  padding: 0
+ h1
+     text-align: center
+ main
+     height: 100%
+     width: 100%
+     margin: 0
+     padding: 0
 </style>
 
 <header></header>
-  
+
 <main><Editor/></main>
 
 <footer></footer>
