29449e9718a489db184667ec67d693b6786cfad4
Eric S. Londres <elondres@stevens.edu>
2020-05-12 14:17:22 -0400

    bottom bar hover

--- frontend/src/BottomBar.svelte
+++ frontend/src/BottomBar.svelte
@@ -1,3 +1,26 @@
-<div id="top-bar">
-    <div id="top-menu-bar"></div>
+<script>
+
+</script>
+
+<style lang="sass">
+#bottom-menu-bar
+     display: none
+     font-family: sans-serif
+#bottom-bar
+     position: fixed
+     bottom: 0
+     height: 10%
+     width: 100%
+     &:hover
+          #bottom-menu-bar
+               display: block
+</style>
+
+<div id="bottom-bar">
+    <div id="bottom-menu-bar">
+	Characters: 0
+	Paragraphs: 0
+	Pages: 0
+	0% of daily goal
+    </div>
 </div>
