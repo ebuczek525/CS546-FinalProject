bfe2304b4e29781182c8db8f62bef673c80ee43a
ebuczek525 <ebuczek@stevens.edu>
2020-05-13 16:15:07 -0400

    Changed code under post method
    added link to return to home for when authentication fails

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,23 +6,20 @@ const users = require('../users');
 router.get('/', (req, res) => res.sendFile('home.html', { root: './' }));
 
 router.post('/login', async (req, res) => {
-    try {
-        let user = JSON.stringify(req.body.username).replace(/"/g, "");
-        let pass = JSON.stringify(req.body.password).replace(/"/g, "");
+    let user = JSON.stringify(req.body.username).replace(/"/g, "");
+    let pass = JSON.stringify(req.body.password).replace(/"/g, "");
 
-        const validLogin = await authenticate(user, pass);
+    const validLogin = await authenticate(user, pass);
 
-        if (validLogin === false) {
-            res.send('You did not provide a valid username and/or password.').status(401);
-            return;
-        }
-
-        req.session.user = validLogin.username;
-        req.session.users = validLogin;
-        res.redirect('/public/index.html');
-    } catch (error) {
-        res.redirect('/');
+    if (validLogin === false) {
+        res.set('Content-Type', 'text/html');
+        res.send(new Buffer('<html><head><title>Bad Login Attempt</title></head><body><p>You did not provide a valid username and/or password.</p><a href="/">Click here to return to the login page.</a></body></html>')).status(401);
+        return;
     }
+
+    req.session.user = validLogin.username;
+    req.session.users = validLogin;
+    res.redirect('/public/index.html');
 });
 
 const authenticate = async (user, pass) => {
