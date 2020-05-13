013eb20a469641366440b8c3880a8fdae0263571
Eric S. Londres <elondres@stevens.edu>
2020-05-13 17:14:21 -0400

    Fixed API call

--- backend/routes/lingua.js
+++ backend/routes/lingua.js
@@ -3,14 +3,41 @@ const router = express.Router();
 
 router.get('/:phrase', async (req, res) => {
     try {
-	const ret = await fetch(`http://lingua-robot.p.rapidapi.com/language/v1/entries/en/${req.params.phrase}`, {
+	var http = require("https");
+	
+	var options = {
 	    "method": "GET",
+	    "hostname": "lingua-robot.p.rapidapi.com",
+	    "port": null,
+	    "path": `/language/v1/entries/en/${req.params.phrase}`,
 	    "headers": {
 		"x-rapidapi-host": "lingua-robot.p.rapidapi.com",
-		"x-rapidapi-key": "66185dfefamshd8a7fc1cbe5b318p186497jsn901b72cdd11f"
+		"x-rapidapi-key": "66185dfefamshd8a7fc1cbe5b318p186497jsn901b72cdd11f",
+		"useQueryString": true
 	    }
+	};
+	
+	var request = http.request(options, response => {
+	    var chunks = [];
+	    
+	    response.on("data", chunk => chunks.push(chunk));
+	
+	    
+	    response.on("end", () => {
+		var body = Buffer.concat(chunks);
+		res.status(200).send(body);
+	    });
 	});
-	res.send(ret);
+	
+	request.end();
+	// const ret = await fetch(`http://lingua-robot.p.rapidapi.com/language/v1/entries/en/${req.params.phrase}`, {
+	//     "method": "GET",
+	//     "headers": {
+	// 	"x-rapidapi-host": "lingua-robot.p.rapidapi.com",
+	// 	"x-rapidapi-key": "66185dfefamshd8a7fc1cbe5b318p186497jsn901b72cdd11f"
+	//     }
+	// });
+	// res.send(ret);
     } catch(e) {
 	res.status(500).send(e.message);
     } finally {
