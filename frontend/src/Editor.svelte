f644d77877fdc23e30f172f9b9b7f02d95512f27
Eric S. Londres <elondres@stevens.edu>
2020-05-13 18:43:04 -0400

    implemented some stuff for text processing

--- frontend/src/Editor.svelte
+++ frontend/src/Editor.svelte
@@ -1,16 +1,38 @@
 <script>
  export let value = '';
  export let selection = '';
+ 
+ async function getUserEmail() {
+     const res = await fetch('/me');
+     console.log(res);
+     if(res.status !== 200) {
+	 console.error(res);
+	 throw new Error(`Error: ${res.status} ${res.statusText}`);
+     } else {
+	 const body = await res.text();
+	 return body;
+     }
+ }
 
-async function onSelect() {
+ async function onSelect() {
      const t = document.getElementById('editor');
      selection = value.substring(t.selectionStart, t.selectionEnd); 
-
+     
      const ret = await ((await fetch(`/lingua/${selection}`)).json());
-
+     
      if(ret.entries.length === 0) {
 	 // lemma does not exist in LinguaRobot
 	 // look in personal user dictionary
+	 try {
+	     const email = await getUserEmail();
+	     const userInfo = await fetch(`/db/${email}`);
+	     console.log(userInfo);
+	     alert('TODO');
+	 } catch(e) {
+	     console.error(e);
+	 } 
+     } else {
+	 
      }
      
  }
