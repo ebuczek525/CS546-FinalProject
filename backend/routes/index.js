85d88f9e34f4140ed2dfb7a3a15e15af50e7a3f7
Eric S. Londres <elondres@stevens.edu>
2020-05-12 22:33:50 -0400

    reorder `use` in constructor

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,8 @@ const userRoutes = require('./users');
 const dbRoutes = require('./AccessDB');
 
 const constructorMethod = (app) => {
-    app.use('/', userRoutes);
     app.use('/db', dbRoutes);
-
+    app.use('/', userRoutes);
     app.use('*', (req, res) => {
         res.redirect('/');
     });
