bbc0a7f6fde2f44fbeb72a9522ab8d4cbcc33b9b
Xuhao Li <37705729+XuhaoLi@users.noreply.github.com>
2020-05-13 14:51:25 -0400

    fix '/login' route fail login will redirect to '/'

--- backend/app.js
+++ backend/app.js
@@ -10,48 +10,48 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(session({
-  name: 'AuthCookie',
-  secret: 'some secret string!',
-  resave: false,
-  saveUninitialized: true
+    name: 'AuthCookie',
+    secret: 'some secret string!',
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use('/public/build/:file', (req, res, next) => {
-    if(req.session.user) {
-	return res.sendFile(req.params.file, { root: __dirname + '/public/build' });
+    if (req.session.user) {
+        return res.sendFile(req.params.file, { root: __dirname + '/public/build' });
     } else {
-	return res.redirect('/');
+        return res.redirect('/');
     }
 });
 
 
 app.use('/public/:file', (req, res, next) => {
-    if(req.session.user) {
-	return res.sendFile(req.params.file, { root: __dirname + '/public' });
+    if (req.session.user) {
+        return res.sendFile(req.params.file, { root: __dirname + '/public' });
     } else {
-	return res.redirect('/');
+        return res.redirect('/');
     }
 });
 
 app.use('/me', (req, res, next) => {
-    if(req.session.user) {
-	return res.send(req.session.user);
+    if (req.session.user) {
+        return res.send(req.session.user);
     } else {
-	return res.status(403).send('forbidden');
+        return res.status(403).send('forbidden');
     }
 });
 
 app.use('/login', (req, res, next) => {
-    if(!req.session.user) {
-	req.method = 'POST';
-	return next();
+    if (!req.session.user) {
+        req.method = 'POST';
+        return next();
     } else {
-	return res.redirect('/public/index.html');
+        return res.redirect('/public/index.html');
     }
 });
 
 configRoutes(app);
 
 app.listen(3000, () => {
-  console.log('Your routes will be running on http://localhost:3000');
+    console.log('Your routes will be running on http://localhost:3000');
 });
