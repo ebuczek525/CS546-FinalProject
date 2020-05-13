9ac6904329c890a819ede240e522829b7f386662
Eric S. Londres <elondres@stevens.edu>
2020-05-13 17:14:30 -0400

    Fixed API call

--- frontend/src/Editor.svelte
+++ frontend/src/Editor.svelte
@@ -6,13 +6,7 @@ async function onSelect() {
      const t = document.getElementById('editor');
      selection = value.substring(t.selectionStart, t.selectionEnd); 
 
-     const ret = await fetch(`http://lingua-robot.p.rapidapi.com/language/v1/entries/en/${selection}`, {
-	 "method": "GET",
-	 "headers": {
-	     "x-rapidapi-host": "lingua-robot.p.rapidapi.com",
-	     "x-rapidapi-key": "66185dfefamshd8a7fc1cbe5b318p186497jsn901b72cdd11f"
-	 }
-     });
+     const ret = await fetch(`/lingua/${selection}`);
      
      console.log(ret);
  }
