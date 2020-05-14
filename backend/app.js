dab40cbfff55a7119d0f0387c50f6e3378da7731
Annmarie DiGioia <annmariedigioia@yahoo.com>
2020-05-13 22:20:22 -0400

    now extras

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,44 @@
-const express = require('express');
-const app = express();
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const configRoutes = require('./routes');
+const bcrypt = require('bcrypt')
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(session({
-  name: 'AuthCookie',
-  secret: 'some secret string!',
-  resave: false,
-  saveUninitialized: true
-}));
+const rounds = 1024     //default amount of rounds
+const password = ''
 
-app.use('/public/build/:file', (req, res, next) => {
-    if(req.session.user) {
-	return res.sendFile(req.params.file, { root: __dirname + '/public/build' });
-    } else {
-	return res.redirect('/');
+//creates hash for password
+bcrypt.hash(password, rounds, function(err, hash) => {
+  if(err){
+    console.error(err)
+    return
+  }
+  console.log(hash)
+
+ /*
+ let hash=bcrypt.hashSync(password, rounds);
+ */
+
+  //compares password and hash to allow for user login
+  bcrypt.compare(password, hash, (err, res) => {
+    if(err){                    //passwords don't match
+      console.error(err)
+      return
     }
-});
+    console.log(res)            //passwords do match
+  })
 
+  /*
+  if(bcrypt.compareSync(password, hash)){
+    console.log(res)
+  }
+  else{
+    console.error(err)
+  }
+*/
 
-app.use('/public/:file', (req, res, next) => {
-    if(req.session.user) {
-	return res.sendFile(req.params.file, { root: __dirname + '/public' });
-    } else {
-	return res.redirect('/');
-    }
-});
+})
 
-app.use('/me', (req, res, next) => {
-    if(req.session.user) {
-	return res.send(req.session.user);
-    } else {
-    res.set('Content-Type', 'text/html');
-    return res.send('<html><head><title>Bad Login Attempt</title></head><body><p>Forbidden. You are not authenticated.</p><a href="/">Click here to return to the login page.</a></body></html>').status(403);
-    }
-});
+const hashPassword = async () => {
+  const hash = await bcrypt.hash(password, rounds)
+  console.log(hash)
+  console.log(await bcrypt.compare(password, hash))
+}
 
-app.use('/login', (req, res, next) => {
-    if(!req.session.user) {
-	req.method = 'POST';
-	return next();
-    } else {
-	return res.redirect('/public/index.html');
-    }
-});
-
-configRoutes(app);
-
-app.listen(3000, () => {
-  console.log('Your routes will be running on http://localhost:3000');
-});
+hashPassword()
