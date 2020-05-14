7826ad432cef907c72cfc5fb1590e49c835e2f5b
ebuczek525 <ebuczek@stevens.edu>
2020-05-13 21:15:19 -0400

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const users = require('../users');
+const users = require('../data/dataUsr');
 
 router.get('/', (req, res) => {
     
